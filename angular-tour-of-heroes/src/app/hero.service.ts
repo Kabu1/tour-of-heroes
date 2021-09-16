147ee1e2fd91cfefd517cf7c671f7c73c239b983
kabu1 <bensonmk99@gmail.com>
2021-09-16 10:01:45 +0300

    debug

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
+
 import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
-import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
-
+  providedIn: 'root',
 })
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
-  getHeroes(id: Number): Observable<Hero[]>{
-    const heroes = HEROES.find(h => history.id === id)!;
-    this.messageService.add('HeroService: fetched heroes id = ${id}');
-    return of(hero);
-  }
 
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
 }
